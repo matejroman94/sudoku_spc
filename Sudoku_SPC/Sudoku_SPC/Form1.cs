090e2d479850e6882efbe8f4eaf349336717e16e
Matej Roman <matej.roman94@gmail.com>
2024-11-17 11:08:15 +0100

    Create solver

--- Sudoku_SPC/Sudoku_SPC/Form1.cs
+++ Sudoku_SPC/Sudoku_SPC/Form1.cs
@@ -1,18 +1,9 @@
 ﻿using Sudoku_SPC.Common;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Sudoku_SPC
 {
